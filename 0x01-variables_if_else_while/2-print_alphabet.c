aa8b04111dfe35cb2496aba61ce0a072f9fb0abc
Seyi-O <seyiolugbami0@gmail.com>
2022-09-15 21:13:49 +0000

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	char a;
 
-	for(a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
 	}
-	putchar('\n');
-	return (0);
+	putchar('\n')
+		return (0);
 }
