b5c544fbd2ccfaaff59341aab640973ede96fc9d
Seyi-O <seyiolugbami0@gmail.com>
2022-09-15 22:09:30 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,10 +5,14 @@
  *Return: Always 0 (Success)
  */
 int main(void)
-{	
-char n;
+{
+	char n;
 
 	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
 	{
 		putchar(n);
 	}
